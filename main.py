f59b7bca3d992fd3727d171177fcef9e653483e2
MikaMa27-meet <82mikaam5@dalet.school>
2026-07-19 18:12:26 +0300

    hiii

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from app1 import run_agent1
-from app2 import run_agent2
+import app1
+import app2
+
+def run_agent1() -> None:
+    app1.run_agent1()
+
+def run_agent2() -> None:
+    app2.run_agent2()
 
 agent_choose = input("Which agent do you want to run?\nAgent1 is strict, dicipline and goes in depth of analayizing context.\nWhile agent2 is more fun and silly, very cutesty and is caring political support mentor.\nType '1' for agent1, type '2' for agent2 : ")
 if agent_choose == '1':
@@ -8,5 +14,3 @@ elif agent_choose == '2':
     run_agent2()
 else:
     print("Invalid input, please try again later.")
-
-                     
