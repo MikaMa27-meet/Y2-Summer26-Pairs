d92e46a1162834c891a75cbff816473571394b80
AliBebsinoPepsi <aaboras276@gmail.com>
2026-07-21 23:16:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 from app1 import run_agent1
 from app2 import run_agent2
+from communi import communi 
 
 while True:
     agent_choose = input("Which agent do you want to run?\nAgent1 is strict, dicipline and goes in depth of analayizing context.\nWhile agent2 is more fun and silly, very cutesty and is caring political support mentor.\n Or you can access a special feature if you have an MUN confrence or a Debate competition, you give the bot your role and topic and it tunrs it into a plan with context and information you need to know, type 3 to use this feature.\nType '1' for agent1, type '2' for agent2, type '3' for the special featrure or 'exit' to exit : ")
@@ -16,7 +17,8 @@ while True:
         break
 
     elif agent_choose == '3':
-        print("you chose 3")
+        topic = input("What topic do you want to turn into a plan? ")
+        communi(topic)
         break
         
     elif agent_choose == 'exit':
